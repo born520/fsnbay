2942fdca665c19d51c8eb8d9b6398b7f8980f614
born520 <born520@gmail.com>
2024-08-19 12:22:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function renderTable(data) {
       }
     }
   });
-  
+
   if (columnWidths.length > 0) {
     const totalWidth = columnWidths.reduce((acc, width) => acc + width, 0);
     const columnWidthPercentages = columnWidths.map(width => (width / totalWidth) * 100);
@@ -68,69 +68,3 @@ function renderTable(data) {
               td.rowSpan = mergedCell.numRows;
               td.colSpan = mergedCell.numColumns;
             }
-          }
-
-          td.style.whiteSpace = 'pre-wrap';
-          tr.appendChild(td);
-        }
-      });
-      table.appendChild(tr);
-    });
-  } else {
-    data.tableData.forEach((row, rowIndex) => {
-      const tr = document.createElement('tr');
-
-      if (data.rowHeights && data.rowHeights[rowIndex]) {
-        tr.style.height = data.rowHeights[rowIndex] + 'px';
-      }
-
-      row.forEach((cellData, colIndex) => {
-        const cellKey = `${rowIndex + 1}-${colIndex + 1}`;
-        const mergeInfo = mergeMap[cellKey];
-
-        if (!mergeInfo || (mergeInfo.masterRow === rowIndex + 1 && mergeInfo.masterColumn === colIndex + 1)) {
-          const td = document.createElement('td');
-
-          if (typeof cellData === 'object') {
-            td.innerHTML = cellData.richText || cellData.text || '';
-          } else {
-            td.innerHTML = cellData;
-          }
-
-          applyStyles(td, rowIndex, colIndex, data);
-
-          if (mergeInfo) {
-            const mergedCell = data.mergedCells.find(cell => cell.row === mergeInfo.masterRow && cell.column === mergeInfo.masterColumn);
-            if (mergedCell) {
-              td.rowSpan = mergedCell.numRows;
-              td.colSpan = mergedCell.numColumns;
-            }
-          }
-
-          td.style.whiteSpace = 'pre-wrap';
-          tr.appendChild(td);
-        }
-      });
-      table.appendChild(tr);
-    });
-  }
-}
-
-function applyStyles(td, rowIndex, colIndex, data) {
-  td.style.backgroundColor = data.backgrounds[rowIndex][colIndex] || '';
-  td.style.color = data.fontColors[rowIndex][colIndex] || '';
-  td.style.textAlign = data.horizontalAlignments[rowIndex][colIndex] || 'left';  // 가로 정렬
-  td.style.verticalAlign = data.verticalAlignments[rowIndex][colIndex] || 'top'; // 세로 정렬
-  td.style.fontWeight = data.fontWeights[rowIndex][colIndex] || 'normal';
-  td.style.fontSize = (data.fontSizes[rowIndex][colIndex] || 12) + 'px';
-
-  if (data.fontStyles[rowIndex][colIndex].includes('strikethrough')) {
-    td.classList.add('strikethrough');
-  }
-
-  applyBorderStyles(td);
-}
-
-function applyBorderStyles(td) {
-  td.style.border = '1px solid black';
-}
